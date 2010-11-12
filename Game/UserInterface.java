d12a95be65f23e076778baf2f82f6c8d8794b328
Kobran <Kobran@.(none)>
2010-11-13 00:13:19 +0100

    hai

--- Game/UserInterface.java
+++ Game/UserInterface.java
@@ -1,14 +1,21 @@
 package Game;
 
-
+import javax.swing.*;
+import java.awt.*;
 /**
  *
  * @author Kobran
  */
 public class UserInterface {
     private GameLogic logic;
+    private JFrame frame = new JFrame();
+    private JLabel lbl = new JLabel("Game");
 
     public UserInterface(GameLogic logic){
         this.logic = logic;
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(lbl);
+        frame.setVisible(true);
     }
 }
